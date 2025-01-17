d4b078b8e7ba568974b3c4312ed725863f1771c7
Jakkapet <jakkapet.wk@gmail.com>
2025-01-18 02:54:21 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: false },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
+  },
+  modules: ['@nuxtjs/tailwindcss']
+  
+})
