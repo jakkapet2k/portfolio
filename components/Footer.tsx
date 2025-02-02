8d0548393328abcbab950c65cda6fa534954ede9
Jakkapet <jakkapet.wk@gmail.com>
2025-02-02 21:21:54 +0700

    update My web

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,7 +14,6 @@ export default function Footer() {
                 <nav className="flex bg-[#0b0b0d]/70 backdrop-blur-lg rounded-lg justify-between items-center h-10 px-5 lg:-px-5 gap-x-5 text-white  w-full  py-10">
                     <a href="/" className="text-base font-bold flex ">Jakkapet<span className="text-yellow-500">./</span></a>
                     <p className=" font-thin text-xs text-nowrap "> <span className="hidden">&copy; Copyright 2025. All Rights Reserved </span> Design by Jakkapet.</p>
-
                 </nav>
             </div>
         </div>
