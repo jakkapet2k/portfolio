c5ba2736d36bb2feaa4e85189e34ea1f2de023a4
Jakkapet <jakkapet.wk@gmail.com>
2025-02-05 17:48:02 +0700

    update resume file and exp web

--- components/Work.tsx
+++ components/Work.tsx
@@ -49,9 +49,31 @@ export default function Work() {
                                         ClickNext co.,ltd.
                                     </h3>
                                     <p className="mt-5 lg:text-base  text-white">
-                                        Interned as a Full Stack Developer, responsible for developing web applications on both the Frontend and Backend.
+                                        Interned as a Full Stack Developer, responsible for developing web applications on both the Front-End and Back-End. Designed and implemented APIs to efficiently connect and transfer data, enhancing features and functionality for users while ensuring seamless communication between systems
                                     </p>
-
+                                    <Accordion type="single" defaultValue="thewin-project" collapsible >
+                                        <AccordionItem value="thewin-project" className="bg-[#0c0c0e] px-3 my-3 rounded-xl">
+                                            <AccordionTrigger className="text-yellow-500 uppercase lg:text-base">
+                                                <p className="flex items-center gap-2"> <span className="size-2 rounded-full bg-yellow-500"></span> Project</p>
+                                            </AccordionTrigger>
+                                            <AccordionContent className="my-3 w-[330px] lg:w-[850px]">
+                                                <div className="space-y-5">
+                                                    <h4 className="text-base font-medium leading-none flex flex-nowrap gap-3"><PiShootingStarFill className="text-yellow-500" />GSB Merchant Back Office </h4>
+                                                    <p className="text- text-gray-400 px-5 ">
+                                                        Developed the Front-End and Back-End of the project. For the Front-End, created a website and features for reporting various store data. For the Back-End, developed APIs to efficiently handle data transfer between the Front-End and Back-End
+                                                    </p>
+                                                </div>
+                                                <Separator className="bg-[#2c2c34] my-3" />
+                                                <div className="flex h-5 items-center space-x-4 text-xs">
+                                                    <div>Nuxt js</div>
+                                                    <Separator orientation="vertical" className="bg-[#2c2c34]" />
+                                                    <div>Vuetify</div>
+                                                    <Separator orientation="vertical" className="bg-[#2c2c34]" />
+                                                    <div> December 2024 - Present</div>
+                                                </div>
+                                            </AccordionContent>
+                                        </AccordionItem>
+                                    </Accordion>
                                 </div>
                             </div>
 
@@ -84,8 +106,8 @@ export default function Work() {
                                     <p className="mt-5 lg:text-base  text-white">
                                         A Frontend Developer primarily focused on developing web applications, with some experience in API Backend development for certain projects.
                                     </p>
-                                    <Accordion type="single" collapsible >
-                                        <AccordionItem value="item-1" className="bg-[#0c0c0e] px-3 my-3 rounded-xl">
+                                    <Accordion type="single" defaultValue="thewin-project" collapsible >
+                                        <AccordionItem value="thewin-project" className="bg-[#0c0c0e] px-3 my-3 rounded-xl">
                                             <AccordionTrigger className="text-yellow-500 uppercase lg:text-base">
                                                 <p className="flex items-center gap-2"> <span className="size-2 rounded-full bg-yellow-500"></span> Project</p>
                                             </AccordionTrigger>
@@ -125,10 +147,9 @@ export default function Work() {
 
                                             <AccordionContent className="my-3 w-[330px] lg:w-[850px]">
                                                 <div className="space-y-5">
-                                                    <h4 className="text-base font-medium leading-none flex flex-nowrap gap-3"><PiShootingStarFill className="text-yellow-500" />Application for management monitoring&reporting fiber
-                                                        optic system</h4>
+                                                    <h4 className="text-base font-medium leading-none flex flex-nowrap gap-3"><PiShootingStarFill className="text-yellow-500" /> Social media application</h4>
                                                     <p className="text- text-gray-400 px-5 ">
-                                                        Worked as a DevOps with Bitbucket and Git server on Ubuntu, and developed the frontend of a web application for administration and technicians
+                                                        Developed the frontend and integrated with the backend API of the web application using NuxtJS, while managing the server on Ubuntu
                                                     </p>
                                                 </div>
                                                 <Separator className="bg-[#2c2c34] my-3" />
@@ -168,8 +189,8 @@ export default function Work() {
                                     <p className="mt-5 lg:text-base  text-white">
                                         University Projects: Developed Frontend for the EDV Metaverse for Sale Digital Land Web Application and EDV NFT Marketplace Project using Next.js
                                     </p>
-                                    <Accordion type="single" collapsible >
-                                        <AccordionItem value="item-1" className="bg-[#0c0c0e] px-3 my-3 rounded-xl">
+                                    <Accordion type="single" defaultValue="university-project" collapsible >
+                                        <AccordionItem value="university-project" className="bg-[#0c0c0e] px-3 my-3 rounded-xl">
                                             <AccordionTrigger className="text-yellow-500 uppercase lg:text-base">
                                                 <p className="flex items-center gap-2"> <span className="size-2 rounded-full bg-yellow-500"></span> Project</p>
                                             </AccordionTrigger>
@@ -177,7 +198,7 @@ export default function Work() {
                                                 <div className="space-y-5">
                                                     <h4 className="text-base font-medium leading-none flex flex-nowrap gap-3"><PiShootingStarFill className="text-yellow-500" />EDV Metaverse for sale digital land web application  </h4>
                                                     <p className="text- text-gray-400 px-5 ">
-                                                        Developed the frontend of websites, integrated APIs, and designed website interfaces.
+                                                        Developed the frontend, integrated APIs from the backend, and designed the website's UI/UX
                                                     </p>
                                                 </div>
                                                 <Separator className="bg-[#2c2c34] my-3" />
@@ -185,12 +206,14 @@ export default function Work() {
                                                 <div className="flex h-5  items-center space-x-4 text-xs">
                                                     <div>Next js</div>
                                                     <Separator orientation="vertical" className="bg-[#2c2c34]" />
+                                                    <div>Tailwind</div>
+                                                    <Separator orientation="vertical" className="bg-[#2c2c34]" />
                                                     <div><a className="flex items-center flex-nowrap gap-3  text-yellow-500 hover:underline" href="https://edvmetaverse.io">edvmetaverse.io <RiExternalLinkLine /></a></div>
                                                 </div>
                                             </AccordionContent>
                                             <AccordionContent className="my-3">
                                                 <div className="space-y-5">
-                                                    <h4 className="text-base font-medium leading-none flex flex-nowrap gap-3"><PiShootingStarFill className="text-yellow-500" />EDV NFT Marketplace Project (Next.js)</h4>
+                                                    <h4 className="text-base font-medium leading-none flex flex-nowrap gap-3"><PiShootingStarFill className="text-yellow-500" />EDV NFT Marketplace Project </h4>
                                                     <p className="text- text-gray-400 px-5 ">
                                                         Developed the frontend of websites, integrated APIs, and designed website interfaces.
                                                     </p>
@@ -198,6 +221,8 @@ export default function Work() {
                                                 <Separator className="bg-[#2c2c34] my-3" />
                                                 <div className="flex h-5  items-center space-x-4 text-xs">
                                                     <div>Next js</div>
+                                                    <Separator orientation="vertical" className="bg-[#2c2c34]" />
+                                                    <div>Tailwind</div>
                                                     <Separator orientation="vertical" className="bg-[#2c2c34]" />
                                                     <div><a className="flex items-center flex-nowrap gap-3  text-yellow-500 hover:underline" href="https://nft.edvmetaverse.io">nft.edvmetaverse.io <RiExternalLinkLine /></a></div>
                                                 </div>
